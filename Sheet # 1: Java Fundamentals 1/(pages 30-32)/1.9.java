e3d83b0797ee117b243cbd5d9d13f68b898d74a7
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-10-07 09:16:16 +0300

    Create 1.9.java

---
+++ Sheet # 1: Java Fundamentals 1/(pages 30-32)/1.9.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+	    float width = 4.5;
+	    float height = 7.9;
+		  System.out.println("The Area: " + height * width);
+		  System.out.println("The Perimeter: " + 2 * (height + width));
+	}
+}
