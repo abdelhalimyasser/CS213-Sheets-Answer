9f63c99cd89e4dcda51b64d5f5a83b84a8147baf
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-10-24 20:20:19 +0300

    Create 5.4.java

---
+++ Sheet # 2: Java Fundamentals 2/(pages 194-204)/5.4.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Miles\tKilometers");
+		for(int i = 1; i <= 10; i++)
+			System.out.println(i + "\t" + (double)i*1.609);
+	}
+}
