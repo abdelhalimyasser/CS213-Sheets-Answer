cb52b86ca1a8cc84baef8427267afd766e4ff60a
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-10-25 11:46:08 +0300

    Create 7.9.java

---
+++ Sheet # 2: Java Fundamentals 2/(pages 280-288)/7.9.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		double[] array = new double[10];
+
+		System.out.print("Enter 10 numbers: ");
+
+		for(int i = 0; i < array.length; i++)
+			array[i] = input.nextDouble();
+
+		System.out.println("The minimum number is " + min(array));
+	}
+
+	public static double min(double[] array) {
+		double min = array[0];
+		for(int i = 0; i < array.length; i++)
+			if(min > array[i])
+				min = array[i];
+		return min;
+	}
+}
