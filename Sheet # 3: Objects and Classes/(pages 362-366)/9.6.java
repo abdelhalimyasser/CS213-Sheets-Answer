67e713c05904de0243fa53e61c04ac1b3a456da7
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-08 23:18:51 +0200

    Create 9.6.java

---
+++ Sheet # 3: Objects and Classes/(pages 362-366)/9.6.java
@@ -0,0 +1,27 @@
+import java.util.GregorianCalendar;
+
+public class Main
+{
+	public static void main(String[] args) {
+		StopWatch stopWatch = new StopWatch();
+	}	
+}
+
+
+public StopWatch{
+    // Class Variabels
+    private int startTime;
+    private int endTime;
+    
+    start(){
+        
+    }
+    
+    stop(){
+        
+    }
+    
+    getElapsedTime(){
+         
+     }
+}
