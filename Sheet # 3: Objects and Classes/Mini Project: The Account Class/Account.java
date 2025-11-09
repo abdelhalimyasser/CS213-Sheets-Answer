e5bb83f11baf86d6ae4c0c0e7d211873df857687
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-10 00:31:41 +0200

    Create Account.java

---
+++ Sheet # 3: Objects and Classes/Mini Project: The Account Class/Account.java
@@ -0,0 +1,48 @@
+class Account {
+	// Class variables
+	private int id;
+	private double balance;
+	private double  annualInterestRate;
+	private date = new Date();
+
+	// Class constractors
+	public Account() {
+		this.id = 0;
+		this.balance = 0;
+		this.annualInterestRate = 0;
+		this.date = new Date();
+	}
+
+	public Account(int id, double balance) {
+		this.id = id;
+		this.balance = balance;
+	}
+
+	// Class setters methods
+	public void setId() {
+	}
+
+	public void setBalance() {
+	}
+
+	public void setAnnualInterestRate() {
+	}
+
+	// Class getters methods
+	public int getId() {
+		return
+	}
+
+	public double getBalance() {
+		return
+	}
+
+	public double getAnnualInterestRate() {
+		return
+	}
+
+	// Class functianol methods
+	getMonthlyInterestRate()
+	withdraw()
+	deposite() {
+	}
