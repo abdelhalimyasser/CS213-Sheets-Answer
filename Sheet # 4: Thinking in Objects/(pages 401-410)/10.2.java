14a2f7f784ac81f80d510967fd897133a1abe8d9
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-21 17:28:23 +0200

    Add BMI class with constructor for attributes

---
+++ Sheet # 4: Thinking in Objects/(pages 401-410)/10.2.java
@@ -0,0 +1,15 @@
+public class BMI{
+    private String name;
+    private int age;
+    private double weight;
+    private double feet;
+    private double inches;
+    
+    public BMI(String name, int age, double weight, double feet, double inches){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.feet = feet;
+        this.inches = inches;
+    }
+}
